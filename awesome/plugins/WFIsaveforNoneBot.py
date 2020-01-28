9fa8e0af7cd68f3ab8650f8b29165cad8af2e367
knight000 <350311089@qq.com>
2020-01-28 20:15:55 +0800

    修复BUG

--- awesome/plugins/WFIsaveforNoneBot.py
+++ awesome/plugins/WFIsaveforNoneBot.py
@@ -17,7 +17,7 @@ def ReadList():
     return data
 
 
-@on_command('Add', aliases=('add', '添加物品'))
+@on_command('Add', aliases=('add', '添加物品'),permission=SUPERUSERS)
 async def Add(session: CommandSession):
     ItemList = ReadList()
     # 从会话状态（session.state）中获取物品名称（item），如果当前不存在，则询问用户
@@ -29,9 +29,10 @@ async def Add(session: CommandSession):
         await session.send("已添加["+item+"]")
     else:
         await session.send("此物品已在列表里")
+    await session.finish()
 
 
-@on_command('Delete', aliases=('del', '删除物品'))
+@on_command('Delete', aliases=('del', '删除物品'),permission=SUPERUSERS)
 async def Delete(session: CommandSession):
     ItemList = ReadList()
     session.state['item'] = session.current_arg_text.strip()
@@ -42,10 +43,12 @@ async def Delete(session: CommandSession):
         await session.send("已删除["+item+"]")
     else:
         await session.send("此物品不在列表里")
+    await session.finish()
 
 
-@on_command('SendItemList', aliases=('item', '查看列表'))
+@on_command('SendItemList', aliases=('item', '查看列表'),permission=SUPERUSERS)
 async def SendItemList(session: CommandSession):
     f = open("awesome\\plugins\\data.list", "r")
     message = "当前已获取列表：\n"+f.read()
     await session.send(message)
+    await session.finish()
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from nonebot.default_config import *
 
 SUPERUSERS = {} #这里填写你自己的QQ号
 # COMMAND_START = {'/', '!', '／', '！'} #这里是默认选项
-COMMAND_START = {'#'} #设置命令以#开头
+COMMAND_START = {'#','!'} #设置命令以#开头
 COMMAND_SEP = {' '} #命令以空格分割
 HOST = '0.0.0.0'
 PORT = 8080 #这里是端口，这里改了http-api那里也要改
