567cf5133afc7943ea9b4e4c297008a513ffc992
knight000 <350311089@qq.com>
2020-01-28 11:33:46 +0800

    修复Dict问题，添加config.py的注释

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from nonebot.default_config import *
 
-SUPERUSERS = {350311089}
+SUPERUSERS = {} #这里填写你自己的QQ号
 # COMMAND_START = {'/', '!', '／', '！'} #这里是默认选项
 COMMAND_START = {'#'}
 HOST = '0.0.0.0'
-PORT = 8080
+PORT = 8080 #这里是端口，这里改了http-api那里也要改
