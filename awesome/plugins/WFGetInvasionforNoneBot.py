6284ac1ae74f25171f729d9150c19e2b8d658a01
knight000 <350311089@qq.com>
2020-01-30 19:55:10 +0800

    version 1.0.4

--- awesome/plugins/WFGetInvasionforNoneBot.py
+++ awesome/plugins/WFGetInvasionforNoneBot.py
@@ -4,20 +4,6 @@ import urllib.request
 from nonebot import on_command, CommandSession
 
 
-def GetZh(name):
-    # 用于翻译
-    str(name)
-    with open("awesome\\plugins\\WF_Dict.json", "r", encoding="UTF-8") as f:
-        # 翻译文件来自https://github.com/Richasy/WFA_Lexicon
-        wfDictList = json.load(f)
-    for list1 in wfDictList:
-        wfDict = dict(list1)
-        if wfDict['en'] == name:
-            return wfDict['zh']
-        else:
-            continue
-
-
 @on_command('Invasion', aliases=('invasion', '入侵'))
 async def Invasion(session: CommandSession):
     url = "https://api.warframestat.us/pc/invasions"
@@ -33,17 +19,34 @@ async def Invasion(session: CommandSession):
         defenderItem = dict(defenderItem[0])
         completion = str(int(invasion['completion']))
         node = invasion['node']
+        with open("awesome\\plugins\\WF_Dict.json", "r", encoding="UTF-8") as f:
+            # 翻译文件来自https://github.com/Richasy/WFA_Lexicon
+            wfDictList = json.load(f)
+        for list1 in wfDictList:
+            wfDict = dict(list1)
+            if wfDict['en'] == defenderItem['type']:
+                ZhdefenderItem = wfDict['zh']
+                break
+            else:
+                continue
         if invasion['completion'] <= 0 or invasion['completion'] >= 100:
             continue
         if invasion['vsInfestation'] == True:
             message += "\n节点:"+node + \
-                "奖励是:["+GetZh(defenderItem['type'])+"]当前进度:"+completion+"%"
+                "奖励是:["+ZhdefenderItem+"]当前进度:"+completion+"%"
         else:
             attacker = dict(invasion['attackerReward'])
             attackerItem = attacker['countedItems']
             attackerItem = dict(attackerItem[0])
+            for list1 in wfDictList:
+                wfDict = dict(list1)
+                if wfDict['en'] == attackerItem['type']:
+                    ZhattackerItem = wfDict['zh']
+                    break
+                else:
+                    continue
             message += "\n节点:"+node+"奖励是:[" + \
-                GetZh(defenderItem['type'])+"]或[" + \
-                GetZh(attackerItem['type'])+"]当前进度:"+completion+"%"
+                ZhdefenderItem+"]或[" + \
+                ZhattackerItem+"]当前进度:"+completion+"%"
     await session.send(message)
     session.finish()
