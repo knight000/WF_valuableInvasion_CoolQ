1f16a8c90c572571a4a8d158506f4e462f0c43c4
knight000 <350311089@qq.com>
2020-02-08 23:06:21 +0800

    version 1.1.3

--- awesome/plugins/WFAlertingforNoneBot.py
+++ awesome/plugins/WFAlertingforNoneBot.py
@@ -4,9 +4,12 @@ from nonebot import on_command, CommandSession
 
 
 async def GetDate(DataType):
-    url = "https://api.warframestat.us/pc/"+DataType
-    data = requests.get(url, verify=False)
-    return data.text
+    try:
+        url = "https://api.warframestat.us/pc/"+DataType
+        data = requests.get(url, verify=False)
+        return data.text
+    except:
+        return False
 
 
 async def GetZh(en):
@@ -53,120 +56,141 @@ async def GetCycleZh(en):
 
 @on_command('voidTrader', aliases=('voidtrader', '虚空商人', 'baro'), only_to_me=False)
 async def voidTrader(session: CommandSession):
-    data = json.loads(await GetDate('voidTrader'))
-    if data['active'] == False:
-        message = "Baro Ki'Teer在路上\n到达剩余时间:"+data['startString']
+    data = await GetDate('voidTrader')
+    if data == False:
+        session.finish('获取信息失败，请稍后再试')
     else:
-        message = "Baro Ki'Teer已到达"+await GetNodeZh(data['location'])
-        inventoryList = data['inventory']
-        for inventory in inventoryList:
-            message += "\n"+await GetZh(inventory['item'])+"\n价格:"
-            if inventory['ducats'] != 0:
-                message += "["+str(inventory['ducats'])+"]杜卡德金币"
-            if inventory['credits'] != 0:
-                message += "["+str(inventory['credits'])+"]现金"
-    await session.send(message)
-    session.finish()
+        data = json.loads(data)
+        if data['active'] == False:
+            message = "Baro Ki'Teer在路上\n到达剩余时间:"+data['startString']
+        else:
+            message = "Baro Ki'Teer已到达"+await GetNodeZh(data['location'])
+            inventoryList = data['inventory']
+            for inventory in inventoryList:
+                message += "\n"+await GetZh(inventory['item'])+"\n价格:"
+                if inventory['ducats'] != 0:
+                    message += "["+str(inventory['ducats'])+"]杜卡德金币"
+                if inventory['credits'] != 0:
+                    message += "["+str(inventory['credits'])+"]现金"
+        session.finish(message)
 
 
 @on_command('Sortie', aliases=('sortie', '突击'), only_to_me=False)
 async def Sortie(session: CommandSession):
-    data = json.loads(await GetDate('sortie'))
-    message = "当前突击为:"
-    for sortie in data['variants']:
-        message += "\n["+await GetZh(sortie['missionType'])+"]"+await GetNodeZh(sortie['node'])+"\n"+await GetModifierZh(sortie['modifier'])
-    message += "\n派系:"+data['faction']+"\n受害者:" + \
-        data['boss']+"\n剩余时间:"+data['eta']
-    await session.send(message)
-    session.finish()
+    data = await GetDate('sortie')
+    if data == False:
+        session.finish('获取信息失败，请稍后再试')
+    else:
+        data = json.loads(data)
+        message = "当前突击为:"
+        for sortie in data['variants']:
+            message += "\n["+await GetZh(sortie['missionType'])+"]"+await GetNodeZh(sortie['node'])+"\n"+await GetModifierZh(sortie['modifier'])
+        message += "\n派系:"+data['faction']+"\n受害者:" + \
+            data['boss']+"\n剩余时间:"+data['eta']
+        session.finish(message)
 
 
 @on_command('Invasion', aliases=('invasion', '入侵'), only_to_me=False)
 async def Invasion(session: CommandSession):
-    data = json.loads(await GetDate('invasions'))
-    message = "当前入侵为："
-    with open("awesome\\plugins\\WF_Invasion.json", "r", encoding="UTF-8") as f:
-        # 翻译文件来自https://github.com/Richasy/WFA_Lexicon
-        wfDictList = json.load(f)
-    for invasion in data:
-        defender = dict(invasion['defenderReward'])
-        defenderItem = defender['countedItems']
-        defenderItem = dict(defenderItem[0])
-        completion = str(int(invasion['completion']))
-        node = await GetNodeZh(invasion['node'])
-        for list1 in wfDictList:
-            wfDict = dict(list1)
-            if wfDict['en'] == defenderItem['type']:
-                ZhdefenderItem = wfDict['zh']
-                break
-            else:
-                continue
-        if invasion['completion'] <= 0 or invasion['completion'] >= 100:
-            continue
-        if invasion['vsInfestation'] == True:
-            message += "\n"+node + \
-                "奖励是:["+ZhdefenderItem+"]\n当前进度:"+completion+"%"
-        else:
-            attacker = dict(invasion['attackerReward'])
-            attackerItem = attacker['countedItems']
-            attackerItem = dict(attackerItem[0])
+    data = await GetDate('invasions')
+    if data == False:
+        session.finish('获取信息失败，请稍后再试')
+    else:
+        data = json.loads(data)
+        message = "当前入侵为："
+        with open("awesome\\plugins\\WF_Invasion.json", "r", encoding="UTF-8") as f:
+            # 翻译文件来自https://github.com/Richasy/WFA_Lexicon
+            wfDictList = json.load(f)
+        for invasion in data:
+            defender = dict(invasion['defenderReward'])
+            defenderItem = defender['countedItems']
+            defenderItem = dict(defenderItem[0])
+            completion = str(int(invasion['completion']))
+            node = await GetNodeZh(invasion['node'])
             for list1 in wfDictList:
                 wfDict = dict(list1)
-                if wfDict['en'] == attackerItem['type']:
-                    ZhattackerItem = wfDict['zh']
+                if wfDict['en'] == defenderItem['type']:
+                    ZhdefenderItem = wfDict['zh']
                     break
                 else:
                     continue
-            message += "\n"+node+"奖励是:[" + \
-                ZhdefenderItem+"]或[" + \
-                ZhattackerItem+"]\n当前进度:"+completion+"%"
-    await session.send(message)
-    session.finish()
+            if invasion['completion'] <= 0 or invasion['completion'] >= 100:
+                continue
+            if invasion['vsInfestation'] == True:
+                message += "\n"+node + \
+                    "奖励是:["+ZhdefenderItem+"]\n当前进度:"+completion+"%"
+            else:
+                attacker = dict(invasion['attackerReward'])
+                attackerItem = attacker['countedItems']
+                attackerItem = dict(attackerItem[0])
+                for list1 in wfDictList:
+                    wfDict = dict(list1)
+                    if wfDict['en'] == attackerItem['type']:
+                        ZhattackerItem = wfDict['zh']
+                        break
+                    else:
+                        continue
+                message += "\n"+node+"奖励是:[" + \
+                    ZhdefenderItem+"]或[" + \
+                    ZhattackerItem+"]\n当前进度:"+completion+"%"
+        session.finish(message)
 
 
 @on_command('Fissures', aliases=('fissures', '裂隙', '裂缝', '虚空裂隙'), only_to_me=False)
 async def Fissures(session: CommandSession):
-    data = json.loads(await GetDate('fissures'))
-    message = "当前裂隙为："
-    for fissure in data:
-        if fissure['expired'] == True:
-            # 过滤掉过期裂隙
-            continue
-        message += "\n["+await GetZh(fissure['tier'])+"]"+await GetZh(fissure['missionType']) + \
-            "\n"+await GetNodeZh(fissure['node'])+"剩余时间:"+fissure['eta']
-    await session.send(message)
-    session.finish()
+    data = await GetDate('fissures')
+    if data == False:
+        session.finish('获取信息失败，请稍后再试')
+    else:
+        data = json.loads(data)
+        message = "当前裂隙为："
+        for fissure in data:
+            if fissure['expired'] == True:
+                # 过滤掉过期裂隙
+                continue
+            message += "\n["+await GetZh(fissure['tier'])+"]"+await GetZh(fissure['missionType']) + \
+                "\n"+await GetNodeZh(fissure['node'])+"剩余时间:"+fissure['eta']
+        session.finish(message)
 
 
 @on_command('Arbitration', aliases=('arbitration', '仲裁'), only_to_me=False)
 async def Arbitration(session: CommandSession):
-    data = json.loads(await GetDate('arbitration'))
-    message = "当前仲裁为:\n"+data['tile']+"("+await GetZh(data['planet'])+")\n"+data['enemy']+await GetZh(data['type'])
-    await session.send(message)
-    session.finish()
-
-
-@on_command('sentientOutposts', aliases=('sentientoutposts', 'S船', '前哨战'), only_to_me=False)
-async def sentientOutposts(session: CommandSession):
-    data = json.loads(await GetDate('sentientOutposts'))
-    if data['active'] == True:
-        mission = dict(data['mission'])
-        message = "sentient前哨战已出现\n节点："+await GetNodeZh(mission['node'])+"\n任务类型："+await GetZh(mission['type'])
+    data = await GetDate('arbitration')
+    if data == False:
+        session.finish('获取信息失败，请稍后再试')
     else:
-        message = "sentient前哨战未出现"
-    await session.send(message)
-    session.finish()
+        data = json.loads(data)
+        message = "当前仲裁为:\n"+data['tile']+"("+await GetZh(data['planet'])+")\n"+data['enemy']+await GetZh(data['type'])
+        session.finish(message)
+
+
+@on_command('sentientOutposts', aliases=('sentientoutposts', 'S船', 's船', '前哨战'), only_to_me=False)
+async def sentientOutposts(session: CommandSession):
+    data = await GetDate('sentientOutposts')
+    if data == False:
+        session.finish('获取信息失败，请稍后再试')
+    else:
+        data = json.loads(data)
+        if data['active'] == True:
+            mission = dict(data['mission'])
+            message = "sentient前哨战已出现\n节点："+await GetNodeZh(mission['node'])+"\n任务类型："+await GetZh(mission['type'])
+        else:
+            message = "sentient前哨战未出现"
+        session.finish(message)
 
 
 @on_command('Cycle', aliases=('cycle', 'wf时间'), only_to_me=False)
 async def Cycle(session: CommandSession):
-    data = json.loads(await GetDate(''))
-    earthCycle = dict(data['earthCycle'])
-    cetusCycle = dict(data['cetusCycle'])
-    vallisCycle = dict(data['vallisCycle'])
-    message = "地球:"+await GetCycleZh(earthCycle['state'])+"\n剩余时间:"+earthCycle['timeLeft']+"\n希图斯:"+await GetCycleZh(cetusCycle['state'])+"\n剩余时间:"+cetusCycle['timeLeft']+"\n福尔图娜:"+await GetCycleZh(vallisCycle['state'])+"\n剩余时间:"+vallisCycle['timeLeft']
-    await session.send(message)
-    session.finish()
+    data = await GetDate('')
+    if data == False:
+        session.finish('获取信息失败，请稍后再试')
+    else:
+        data = json.loads(data)
+        earthCycle = dict(data['earthCycle'])
+        cetusCycle = dict(data['cetusCycle'])
+        vallisCycle = dict(data['vallisCycle'])
+        message = "地球:"+await GetCycleZh(earthCycle['state'])+"\n剩余时间:"+earthCycle['timeLeft']+"\n希图斯:"+await GetCycleZh(cetusCycle['state'])+"\n剩余时间:"+cetusCycle['timeLeft']+"\n福尔图娜:"+await GetCycleZh(vallisCycle['state'])+"\n剩余时间:"+vallisCycle['timeLeft']
+        session.finish(message)
 
 
 @on_command('RivenData', aliases=('riven', '倾向'), only_to_me=False)
--- awesome/plugins/WFInvasionforNoneBot.py
+++ awesome/plugins/WFInvasionforNoneBot.py
@@ -5,9 +5,12 @@ import nonebot
 
 
 async def GetDate(DataType):
-    url = "https://api.warframestat.us/pc/"+DataType
-    data = requests.get(url, verify=False)
-    return data.text
+    try:
+        url = "https://api.warframestat.us/pc/"+DataType
+        data = requests.get(url, verify=False)
+        return data.text
+    except:
+        return False
 
 
 async def GetZh(en):
@@ -37,65 +40,69 @@ async def _():
     f.close()
     Gotlist = Gotlist.splitlines()
     ReturnData = ""
-    invasions = json.loads(await GetDate('invasions'))
-    global RepeatID
-    with open("awesome\\plugins\\WF_Invasion.json", "r", encoding="UTF-8") as f:
-        # 翻译文件来自https://github.com/Richasy/WFA_Lexicon
-        wfDictList = json.load(f)
-    for dict1 in invasions:
-        #dict1 = dict(a)
-        if dict1['id'] in RepeatID:
-            # 去重
-            continue
-        else:
-            RepeatID.add(dict1['id'])
-        if dict1['completion'] <= 0 or dict1['completion'] >= 100:
-            # RepeatID.remove(dict1['id'])  # 去掉已过期的入侵，但是好像没必要就注释掉了
-            continue
-        if dict1['vsInfestation'] == True:
-            # 过滤掉I系相关的入侵，因为没有部件
-            continue
-        attacker = dict(dict1['attackerReward'])
-        attackerItemDict = attacker['countedItems']
-        attackerItemDict = dict(attackerItemDict[0])
-        defender = dict(dict1['defenderReward'])
-        defenderItemDict = defender['countedItems']
-        defenderItemDict = dict(defenderItemDict[0])
-        # 这里是汉化
-        attackerItem = attackerItemDict['type']
-        defenderItem = defenderItemDict['type']
-        for list1 in wfDictList:
-            wfDict = dict(list1)
-            if wfDict['en'] == attackerItem:
-                attackerItem = wfDict['zh']
-                break
-            else:
+    invasions = await GetDate('invasions')
+    if invasions == False:
+        print('获取信息失败')
+    else:
+        invasions = json.loads(invasions)
+        global RepeatID
+        with open("awesome\\plugins\\WF_Invasion.json", "r", encoding="UTF-8") as f:
+            # 翻译文件来自https://github.com/Richasy/WFA_Lexicon
+            wfDictList = json.load(f)
+        for dict1 in invasions:
+            #dict1 = dict(a)
+            if dict1['id'] in RepeatID:
+                # 去重
                 continue
-        for list2 in wfDictList:
-            wfDict = dict(list2)
-            if wfDict['en'] == defenderItem:
-                defenderItem = wfDict['zh']
-                break
             else:
+                RepeatID.add(dict1['id'])
+            if dict1['completion'] <= 0 or dict1['completion'] >= 100:
+                # RepeatID.remove(dict1['id'])  # 去掉已过期的入侵，但是好像没必要就注释掉了
                 continue
-        # 这里是已拥有的就不提示
-        if attackerItem in Gotlist and defenderItem in Gotlist:
-            continue
-        # if dict1['rewardTypes'] in highvalue:
-        if attackerItemDict['count'] == 1 or defenderItemDict['count'] == 1:
-            # 用奖励的数量来判断是不是武器部件
-            node = await GetNodeZh(dict1['node'])
-            completion = str(int(dict1['completion']))
-            ReturnData += "\n"+node+"，奖励是:[" + \
-                attackerItem+']和[' + \
-                defenderItem+"]\n当前进度："+completion+"%"
-            if attackerItem in Gotlist:
-                ReturnData += "\n其中["+attackerItem+"]已拥有"
-            if defenderItem in Gotlist:
-                ReturnData += "\n其中["+defenderItem+"]已拥有"
-    f = open("QQ.txt", "r")
-    QQ = int(f.readline())
-    f.close
-    if ReturnData != "":
-        ReturnData = "发现高价值入侵:"+ReturnData
-        await bot.send_private_msg(user_id=QQ, message=ReturnData)
+            if dict1['vsInfestation'] == True:
+                # 过滤掉I系相关的入侵，因为没有部件
+                continue
+            attacker = dict(dict1['attackerReward'])
+            attackerItemDict = attacker['countedItems']
+            attackerItemDict = dict(attackerItemDict[0])
+            defender = dict(dict1['defenderReward'])
+            defenderItemDict = defender['countedItems']
+            defenderItemDict = dict(defenderItemDict[0])
+            # 这里是汉化
+            attackerItem = attackerItemDict['type']
+            defenderItem = defenderItemDict['type']
+            for list1 in wfDictList:
+                wfDict = dict(list1)
+                if wfDict['en'] == attackerItem:
+                    attackerItem = wfDict['zh']
+                    break
+                else:
+                    continue
+            for list2 in wfDictList:
+                wfDict = dict(list2)
+                if wfDict['en'] == defenderItem:
+                    defenderItem = wfDict['zh']
+                    break
+                else:
+                    continue
+            # 这里是已拥有的就不提示
+            if attackerItem in Gotlist and defenderItem in Gotlist:
+                continue
+            # if dict1['rewardTypes'] in highvalue:
+            if attackerItemDict['count'] == 1 or defenderItemDict['count'] == 1:
+                # 用奖励的数量来判断是不是武器部件
+                node = await GetNodeZh(dict1['node'])
+                completion = str(int(dict1['completion']))
+                ReturnData += "\n"+node+"，奖励是:[" + \
+                    attackerItem+']和[' + \
+                    defenderItem+"]\n当前进度："+completion+"%"
+                if attackerItem in Gotlist:
+                    ReturnData += "\n其中["+attackerItem+"]已拥有"
+                if defenderItem in Gotlist:
+                    ReturnData += "\n其中["+defenderItem+"]已拥有"
+        f = open("QQ.txt", "r")
+        QQ = int(f.readline())
+        f.close
+        if ReturnData != "":
+            ReturnData = "发现高价值入侵:"+ReturnData
+            await bot.send_private_msg(user_id=QQ, message=ReturnData)
